bb1eb3ebb293d6f051896b311fc5bddd7d6529dd
ajsherrell <ajsherrell.ajacks@gmail.com>
2020-05-15 14:39:04 -0400

    storing

--- app/src/main/java/com/ajsherrell/android/quiz_hyperbaric/DetailFragment.kt
+++ app/src/main/java/com/ajsherrell/android/quiz_hyperbaric/DetailFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
@@ -75,7 +76,6 @@ class DetailFragment : Fragment() {
         rootView = binding.root
         return rootView
     }
-
 }
 
 
