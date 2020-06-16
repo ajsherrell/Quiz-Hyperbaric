a5996f88ec5a7a112ee124be221ee73cfcb2a124
ajsherrell <ajsherrell.ajacks@gmail.com>
2020-06-16 12:16:00 -0400

    add splash screen

---
+++ app/src/main/java/com/ajsherrell/android/quiz_hyperbaric/nav/SplashActivity.kt
@@ -0,0 +1,28 @@
+package com.ajsherrell.android.quiz_hyperbaric.nav
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ajsherrell.android.quiz_hyperbaric.MainActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    //used: https://medium.com/@ssaurel/create-a-splash-screen-on-android-the-right-way-93d6fb444857
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val intent = Intent(this, MainActivity::class.java)
+        val timer = object : Thread() {
+            override fun run() {
+                try {
+                    sleep(3000)
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                } finally {
+                    startActivity(intent)
+                    finish()
+                }
+            }
+        }
+        timer.start()
+    }
+}
