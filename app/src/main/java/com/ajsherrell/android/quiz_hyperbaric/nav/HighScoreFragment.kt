c3588bbf01d2a64f709520a478a3d315f1599d82
ajsherrell <ajsherrell.ajacks@gmail.com>
2020-06-19 15:27:43 -0400

    add license

--- app/src/main/java/com/ajsherrell/android/quiz_hyperbaric/nav/HighScoreFragment.kt
+++ app/src/main/java/com/ajsherrell/android/quiz_hyperbaric/nav/HighScoreFragment.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2020 AJ Sherrell
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.ajsherrell.android.quiz_hyperbaric.nav
 
 import android.os.Bundle
@@ -97,7 +112,6 @@ class HighScoreFragment : Fragment() {
             model.sbCat.append("\n")
         }
         for (i in 0 until model.scores.size) {
-            model.sbScore.append(resources.getString(R.string.separator))
             model.sbScore.append(" ")
             model.sbScore.append(model.scores[i])
             model.sbScore.append("\n")
